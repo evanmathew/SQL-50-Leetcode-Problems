c4ec81054fc582120d0c1c01ed5c090292194b6c
Evan Saju Mathew <evanptc@gmail.com>
2024-10-29 20:15:10 +0530

    1757. Recyclable and Low Fat Products.sql

--- 1757. Recyclable and Low Fat Products.sql
+++ 1757. Recyclable and Low Fat Products.sql
@@ -1,3 +1,2 @@
-# Write your MySQL query statement below
-select product_id from products 
-where low_fats = 'Y' AND recyclable = 'Y'
+SELECT product_id FROM products 
+WHERE low_fats = 'Y' AND recyclable = 'Y'
