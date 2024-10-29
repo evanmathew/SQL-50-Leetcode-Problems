52534f645a5f3a503d985b111116b912c86baa1d
Evan Saju Mathew <evanptc@gmail.com>
2024-10-29 20:13:19 +0530

    1280. Students and Examinations.sql

---
+++ 1280. Students and Examinations.sql
@@ -0,0 +1,6 @@
+SELECT st.student_id, st.student_name ,sb.subject_name, COUNT(ex.subject_name) AS attended_exams 
+FROM Students st
+JOIN Subjects sb
+LEFT JOIN Examinations ex ON st.student_id = ex.student_id AND sb.subject_name = ex.subject_name
+GROUP BY st.student_id , st.student_name , sb.subject_name
+ORDER BY st.student_id , st.student_name , sb.subject_name
