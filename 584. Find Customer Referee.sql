992a0531667193135e1f86a20ae694297686efef
Evan Saju Mathew <evanptc@gmail.com>
2024-10-29 20:15:44 +0530

    584. Find Customer Referee.sql

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,3 +1,2 @@
-# Write your MySQL query statement below
-select name from Customer
-where (referee_id IS NULL) or (NOT referee_id = 2)
+SELECT name FROM Customer
+WHERE (referee_id IS NULL) OR (NOT referee_id = 2)
