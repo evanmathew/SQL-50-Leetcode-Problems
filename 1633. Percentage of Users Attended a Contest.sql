5c873b97c83be1066f38a26e49308db151578c29
Evan Saju Mathew <evanptc@gmail.com>
2024-10-30 00:15:59 +0530

    1633. Percentage of Users Attended a Contest.sql

--- 1633. Percentage of Users Attended a Contest.sql
+++ 1633. Percentage of Users Attended a Contest.sql
@@ -1,5 +1,4 @@
 SELECT r.contest_id, ROUND((COUNT(DISTINCT r.user_id) / (SELECT COUNT(DISTINCT user_id) FROM Users)) * 100, 2) AS percentage
 FROM Users u
-LEFT JOIN Register r ON u.user_id = r.user_id 
 GROUP BY r.contest_id
 ORDER BY percentage DESC, contest_id ASC
