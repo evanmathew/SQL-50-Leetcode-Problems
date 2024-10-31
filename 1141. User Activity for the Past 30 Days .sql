f7363a4492c9db619c7bfd00b1b171d02b9c1277
Evan Saju Mathew <evanptc@gmail.com>
2024-11-01 00:22:48 +0530

    1141. User Activity for the Past 30 Days .sql

---
+++ 1141. User Activity for the Past 30 Days .sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT activity_date AS day,count(DISTINCT user_id) AS active_users FROM Activity 
+WHERE (DATEDIFF('2019-07-27',activity_date)<30)  AND  (activity_date <= '2019-07-27')
+GROUP BY activity_date
